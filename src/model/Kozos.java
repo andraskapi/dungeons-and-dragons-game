8fdf37ecbbebecb494c805504f05baad8daf931b
andra <andras_kapitany@hotmail.hu>
2023-06-10 16:57:40 +0200

    character and monster updated_v2

---
+++ src/model/Kozos.java
@@ -0,0 +1,27 @@
+package model;
+
+public class Kozos {
+    private String name;
+    private int health;
+
+    public Kozos (String name, int health){
+        this.name = name;
+        this.health = health;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+}
--- src/model/Monster.java
+++ src/model/Monster.java
@@ -1,42 +1,32 @@
 package model;
 
-public class Monster {
+public class Monster extends Kozos {
 
     // Vegyél fel két fieldet: name (String) és health (int)
     private String name;
     private int health;
 
     // Írj egy két paraméteres kosntruktort ami beállítja az attribútumok értékeit!
-    public Monster (String name, int health, int level){
-        this.name = name;
-        this.health = 100;
-
+    public Monster (String name, int health){
+      super(name,health);
     }
 
     public void attack(Character player) {
         // Írd ki: [szörny neve] attacks [játékos neve]!
+        System.out.println(name + " attacks " + player.getName());
 
         // Csökkentsd a játékos életét 20-szal!
+        player.setHealth(getHealth() - 20);
 
         // Írd ki: Dealt 20 damage to [játékos neve]!
+        System.out.println("Dealt 20 damage to " + player.getName());
 
         // Ha a játékos élete 0 vagy kisebb, írd ki: [játékos neve] has been defeated!
+        if (player.getHealth() <= 0){
+            System.out.println(player.getName() + " has been defeated!");
+        }
 
     }
 
-    public String getName() {
-        return name;
-    }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getHealth() {
-        return health;
-    }
-
-    public void setHealth(int health) {
-        this.health = health;
-    }
 }
